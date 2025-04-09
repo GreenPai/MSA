261560516971b3a799a70de2c049d9cb9f80eefe
GreenPai <clsrntkdgh@gmail.com>
2025-04-09 15:23:56 +0900

    Spring Cloud Config

---
+++ spring-cloub-service1/src/main/java/kr/co/greenpai/SpringCloubService1Application.java
@@ -0,0 +1,13 @@
+package kr.co.greenpai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloubService1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloubService1Application.class, args);
+    }
+
+}
